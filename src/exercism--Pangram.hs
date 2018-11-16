4cb8dd2826703100d7d98be3104a2c59adeba2da
Dan Davison <dandavison7@gmail.com>
2018-11-16 10:16:02 -0500

    Update Pangram to match required API

--- src/exercism--Pangram.hs
+++ src/exercism--Pangram.hs
@@ -1,39 +1,50 @@
+module Pangram (isPangram) where
+
+import Data.Char (toLower)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Test.Hspec (hspec, describe, it, shouldBe)
 
-isPangram :: String -> String -> Bool
-isPangram string alphabet = isPangram' string (Set.fromList alphabet)
+-- The API called for in the exercise
+isPangram :: String -> Bool
+isPangram string = isPangram' string "abcdefghijklmnopqrstuvwxyz"
 
-isPangram' :: String -> Set Char -> Bool
-isPangram' string alphabet
+-- A more general function that allows the alphabet to be specified as a string
+isPangram' :: String -> String -> Bool
+isPangram' string alphabet = isPangram'' (toLower <$> string) (Set.fromList $ toLower <$> alphabet)
+
+-- A private helper function that does the actual work
+isPangram'' :: String -> Set Char -> Bool
+isPangram'' string alphabet
   | alphabet == Set.empty = True
-  | string == [] = False
-  | otherwise = isPangram' xs (Set.delete x alphabet)
+  | string == "" = False
+  | otherwise = isPangram'' xs (Set.delete x alphabet)
   where
     (x:xs) = string
 
 main :: IO ()
 main = hspec $ do
-  describe "isPangram" $ do
+  describe "isPangram'" $ do
 
     it "all empty" $ do
-      isPangram "" "" `shouldBe` True
+      isPangram' "" "" `shouldBe` True
 
     it "empty alphabet" $ do
-      isPangram "a" "" `shouldBe` True
+      isPangram' "a" "" `shouldBe` True
 
     it "empty string" $ do
-      isPangram "" "a" `shouldBe` False
+      isPangram' "" "a" `shouldBe` False
 
     it "a a" $ do
-      isPangram "a" "a" `shouldBe` True
+      isPangram' "a" "a" `shouldBe` True
 
     it "a b" $ do
-      isPangram "a" "b" `shouldBe` False
+      isPangram' "a" "b" `shouldBe` False
 
     it "ab b" $ do
-      isPangram "ab" "b" `shouldBe` True
+      isPangram' "ab" "b" `shouldBe` True
 
     it "The quick brown fox jumps over the lazy dog" $ do
-      isPangram "the quick brown fox jumps over the lazy dog" "abcdefghijklmnopqrstuvwxyz" `shouldBe` True
+      isPangram'
+        "the quick brown fox jumps over the lazy dog"
+        "abcdefghijklmnopqrstuvwxyz" `shouldBe` True
