b2e3a8bfd3966afab3ee2017ba0272f8cb429583
Dan Davison <dandavison7@gmail.com>
2018-11-26 18:05:05 -0500

    Improve collatzSequence

--- src/exercism--CollatzConjecture.hs
+++ src/exercism--CollatzConjecture.hs
@@ -1,25 +1,17 @@
 module CollatzConjecture (collatz) where
 
+import Data.List (elemIndex)
+
 
 collatz :: Integer -> Maybe Integer
 collatz n
-  | n >= 1 = firstIndexWhere (== 1) (collatzSequence n)
+  | n >= 1 = toInteger <$> elemIndex 1 (collatzSequence n)
   | otherwise = Nothing
 
 
 collatzSequence :: Integer -> [Integer]
 collatzSequence n = n : (collatzSequence next)
   where
-    next = if n `mod` 2 == 0
-           then n `div` 2
-           else 3 * n + 1
-
-
-firstIndexWhere :: Num a => (a -> Bool) -> [a] -> Maybe Integer
-firstIndexWhere = firstIndexWhere' 0
-  where
-    firstIndexWhere' _ _ [] = Nothing
-    firstIndexWhere' offset f (x:xs) = if f x
-                                       then Just offset
-                                       else firstIndexWhere' (offset + 1) f xs
-
+    next
+      | even n = n `div` 2
+      | otherwise = 3 * n + 1
