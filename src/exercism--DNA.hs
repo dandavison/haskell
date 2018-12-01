b3073f14ee0876ce6214a0df03a26d2aaf40ad99
Dan Davison <dandavison7@gmail.com>
2018-12-01 11:35:26 -0500

    WIP Exercism DNA

--- src/exercism--DNA.hs
+++ src/exercism--DNA.hs
@@ -1,4 +1,20 @@
 module DNA (toRNA) where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+
+DNA2RNA :: Map Char Char
+DNA2RNA = Map.fromList
+  [
+    'G': 'C',
+    'C': 'G',
+    'T': 'A'
+    'A': 'U'
+  ]
+
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA (x:xs) = case Map.lookup x DNA2RNA of
+  Just rnaNucleotide -> Right $ rnaNucleotide : toRNA xs
+  Nothing -> Left x
