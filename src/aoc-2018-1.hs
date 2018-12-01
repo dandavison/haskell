592ba504ceacf98a4cb82d8fe808dc40bb69fbce
Dan Davison <dandavison7@gmail.com>
2018-12-01 14:28:25 -0500

    aoc 1.2

--- src/aoc-2018-1.hs
+++ src/aoc-2018-1.hs
@@ -1,3 +1,5 @@
+import qualified Data.Set as Set
+import Data.Set (Set)
 import System.IO (getLine, isEOF)
 
 
@@ -24,8 +26,30 @@ parseLine ('+':xs) = read xs :: Integer
 parseLine xs = read xs :: Integer
 
 
+firstRepeatedValue :: Ord a => [a] -> Set a -> a
+firstRepeatedValue (x:xs) seen =
+  if (Set.member x seen)
+  then x
+  else firstRepeatedValue xs (Set.union seen (Set.singleton x))
+
+
+cumulativeSum :: Num a => a -> [a] -> [a]
+cumulativeSum start (x:xs) = next : (cumulativeSum next xs)
+  where
+    next = start + x
+cumulativeSum start [] = [start]
+
+
+firstRepeatedCumulativeSum :: (Ord a, Num a) => [a] -> a
+firstRepeatedCumulativeSum values =
+  firstRepeatedValue (cumulativeSum 0 (cycle values)) (Set.empty)
+
+
 main :: IO ()
 main = do
   -- values <- getValues []
   values <- getValues'
+  -- part 1
   print $ foldr (+) 0 values
+  -- part 2
+  print $ firstRepeatedCumulativeSum values
