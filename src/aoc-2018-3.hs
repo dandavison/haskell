c77aa431d4c287dde6c2591c948213b7768e2e9a
Dan Davison <dandavison7@gmail.com>
2018-12-03 02:21:59 -0500

    Handle multiple counting

--- src/aoc-2018-3.hs
+++ src/aoc-2018-3.hs
@@ -1,6 +1,11 @@
+import Data.Maybe (fromMaybe)
+import qualified Data.Map as Map
+import Data.Map (Map, (!?))
 import Data.List.Split
 
 
+type Point = (Integer, Integer)
+type Interval = (Integer, Integer)
 type Rectangle = (Integer, Integer, Integer, Integer)
 
 
@@ -18,24 +23,48 @@ parseLine line = (x, y, dx, dy)
     allButLast = reverse . tail . reverse
 
 
-part1 :: [Rectangle] -> Integer
-part1 rectangles = foldr (+) 0 (map overlapSize (unorderedPairs rectangles))
+part1 :: [Rectangle] -> Int
+part1 rectangles =
+  length [ (point, count) | (point, count) <- Map.toList overlapCounts, count > 1 ]
+  where
+    overlapCounts = getOverlapCounts (unorderedPairs rectangles)
 
 
-overlapSize :: (Rectangle, Rectangle) -> Integer
-overlapSize (r1, r2) = (xOverlap r1 r2) * (yOverlap r1 r2)
+getOverlapCounts :: [(Rectangle, Rectangle)] -> Map Point Integer
+getOverlapCounts pairs = getOverlapCounts' Map.empty pairs
 
 
-xOverlap :: Rectangle -> Rectangle -> Integer
-xOverlap r1 r2 = maximum [overlap, 0]
+getOverlapCounts' :: Map Point Integer -> [(Rectangle, Rectangle)] -> Map Point Integer
+getOverlapCounts' counts [] = counts
+getOverlapCounts' counts (pair:pairs) =
+  increment counts (getOverlappingPoints (fst pair) (snd pair))
+  where
+    increment counts [] = counts
+    increment counts (p:ps) = increment (Map.insert p (1 + (fromMaybe 1 (counts !? p))) counts) ps
+
+
+getOverlappingPoints :: Rectangle -> Rectangle -> [Point]
+getOverlappingPoints r1 r2 = [(i, j) |
+                               i <- [(fst xo)..((snd xo) - 1)],
+                               j <- [(fst yo)..((snd yo) - 1)]]
+  where
+    xo = xOverlap r1 r2
+    yo = yOverlap r1 r2
+
+
+-- Hack: returning (n, n-1) tuple in case of no overlap yields empty list [n..(n-1)] of points in
+-- interval.
+
+xOverlap :: Rectangle -> Rectangle -> Interval
+xOverlap r1 r2 = if overlap > 0 then (x2, x1 + dx1) else (x2, x2 - 1)
   where
     overlap = x1 + dx1 - x2
     (x1, _, dx1, _) = minimum [r1, r2]
     (x2, _, dx2, _) = maximum [r1, r2]
 
 
-yOverlap :: Rectangle -> Rectangle -> Integer
-yOverlap r1 r2 = maximum [overlap, 0]
+yOverlap :: Rectangle -> Rectangle -> Interval
+yOverlap r1 r2 = if overlap > 0 then (y2, y1 + dy1) else (y2, y2 - 1)
   where
     overlap = y1 + dy1 - y2
     (_, y1, _, dy1) = if y1' < y2' then r1 else r2
@@ -51,5 +80,15 @@ unorderedPairs xs = [(xs !! i, xs !! j) | i <- [0..(n - 1)], j <- [0..(n - 1)], 
 
 
 main = do
+  -- let rectangles = map parseLine $ lines "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"
+  -- print $ xOverlap (rectangles !! 0) (rectangles !! 1)
+  -- print $ yOverlap (rectangles !! 0) (rectangles !! 1)
+  -- print $ getOverlappingPoints (rectangles !! 0) (rectangles !! 1)
+  -- print $ Map.toList $ getOverlapCounts (unorderedPairs rectangles)
+  -- print $ part1 rectangles
+
   rectangles <- getInput
+  -- print $ xOverlap (rectangles !! 0) (rectangles !! 1)
+  -- print $ yOverlap (rectangles !! 0) (rectangles !! 1)
+  -- print $ getOverlappingPoints (rectangles !! 0) (rectangles !! 1)
   print $ part1 rectangles
