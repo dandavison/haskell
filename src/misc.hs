4163588c1ff22768a53df4c11b90ef41d70254c7
Dan Davison <dandavison7@gmail.com>
2018-12-08 22:28:27 -0500

    Misc

--- src/misc.hs
+++ src/misc.hs
@@ -2,3 +2,13 @@ take' :: Integer -> [a] -> [a]
 take' _ [] = []
 take' 0 (x:xs) = []
 take' n (x:xs) = x : (take' (n - 1) xs)
+
+
+length' :: [a] -> Integer
+length' xs = toInteger $ length xs
+
+take' :: Integer -> [a] -> [a]
+take' n = take' (fromInteger n)
+
+drop' :: Integer -> [a] -> [a]
+drop' n = drop (fromInteger n)
