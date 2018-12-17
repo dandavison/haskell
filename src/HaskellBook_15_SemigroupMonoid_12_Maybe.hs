1c25d6565a64cf9e050e6788648223272226f162
Dan Davison <dandavison7@gmail.com>
2018-12-17 00:28:34 -0500

    HaskellBook_15_SemigroupMonoid_12_Maybe.hs

--- src/HaskellBook_15_SemigroupMonoid_12_Maybe.hs
+++ src/HaskellBook_15_SemigroupMonoid_12_Maybe.hs
@@ -1,12 +1,49 @@
 -- Write a Monoid instance for a Maybe type which doesn’t require a Monoid for the contents. Reuse
 -- the Monoid law QuickCheck properties and use them to validate the instance.
-import HaskellBook_15_SemigroupMonoid_10_Optional
 
--- newtype First' a = Only a | Nada
+-- import HaskellBook_15_SemigroupMonoid_10_Optional
+
+-- newtype First' a =
+--   First' { getFirst :: Optional a }
+--   deriving (Eq, Show)
+
+import Test.QuickCheck
 
 
--- instance Monoid First' where
---   mempty = Nada
+data First' a = Just' a | Nothing' deriving (Eq, Show)
+
+
+instance Semigroup (First' a) where
+
+  (Just' x) <> _ = (Just' x)
+  Nothing' <> (Just' y) = (Just' y)
+  Nothing' <> Nothing' = Nothing'
+
+
+instance Monoid (First' a) where
+  mempty = Nothing'
+
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = do
+    x <- arbitrary
+    return (Just' x)
+
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc x y z = x <> (y <> z) == (x <> y) <> z
+
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstMappend =
+     First' String
+  -> First' String
+  -> First' String
+  -> Bool
+
+type FstId = First' String -> Bool
+
 
 main = do
-  print $ Only "hello"
+  quickCheck (semigroupAssoc :: FirstMappend)
